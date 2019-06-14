5a4409d110ef86a0cb748bcfbe9cac66ae109cb0
Shinichiro Todaka <shinichiro.tdk@gmail.com>
2019-06-14 14:29:02 +0900

    init (#1)

---
+++ src/typings/globals.d.ts
@@ -0,0 +1,9 @@
+// typings/globals.d.ts
+
+declare interface Window {
+  __REDUX_DEVTOOLS_EXTENSION__: any
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any
+}
+
+declare module '*.scss'
+declare module '*.css'
