6916721a81c639873504f7597b48412ade236f75
Shinichiro Todaka <shinichiro.tdk@gmail.com>
2019-06-14 16:05:40 +0900

    implement counter page

---
+++ src/pages/Routes/Counter/Counter.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { Statistic, Button } from 'semantic-ui-react'
+
+import { ReducerState } from '../../../store/root-reducer'
+import {
+  incrementCounter,
+  decrementCounter,
+  resetCounter
+} from '../../../features/counter/widget'
+import * as style from './Counter.scss'
+
+const CounterComponent = () => {
+  const dispatch = useDispatch()
+  const count = useSelector((state: ReducerState) => state.counter.count)
+  return (
+    <div>
+      <div className={style.counter}>
+        <Statistic>
+          <Statistic.Value>{count}</Statistic.Value>
+          <Statistic.Label>Counts</Statistic.Label>
+        </Statistic>
+      </div>
+      <Button.Group>
+        <Button onClick={() => dispatch(incrementCounter())}>+1</Button>
+        <Button onClick={() => dispatch(decrementCounter())}>-1</Button>
+        <Button onClick={() => dispatch(resetCounter())}>Reset 0</Button>
+      </Button.Group>
+    </div>
+  )
+}
+
+export default withRouter(CounterComponent)
--- src/pages/Routes/Home/Home.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import { withRouter } from 'react-router-dom'
-
-const HomeComponent = () => <div>hello!</div>
-
-export const HomePage = withRouter(HomeComponent)
