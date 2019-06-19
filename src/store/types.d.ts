f028732f0a215b7d5017ba1227f8e9a979c7289d
Shinichiro Todaka <shinichiro.tdk@gmail.com>
2019-06-19 16:04:03 +0900

    fix types (#3)

--- src/store/types.d.ts
+++ src/store/types.d.ts
@@ -2,8 +2,9 @@ import { StateType } from 'typesafe-actions'
 import { RouterAction } from 'connected-react-router'
 
 import rootReducer from './root-reducer'
+import { CounterAction } from '@/features/counter/widget'
 
 declare module 'Types' {
   export type RootState = StateType<typeof rootReducer>
-  export type RootAction = RouterAction
+  export type RootAction = RouterAction | CounterAction
 }
